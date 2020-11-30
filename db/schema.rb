7cd89f26c8d9bac8c5a9a95a6bba294488a33bd2
Zaid Badiger <badiger.2@osu.edu>
2020-11-30 13:38:38 -0500

    added email attribute to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_182308) do
+ActiveRecord::Schema.define(version: 2020_11_30_183712) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2020_11_30_182308) do
     t.integer "section_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
