635bbcfbbcd3d90f2fc2281d67f9d1af048162e4
Antoni <rempala.5@buckeyemail.osu.edu>
2020-12-01 22:42:39 -0500

    Added initial login functionality for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 
 end
