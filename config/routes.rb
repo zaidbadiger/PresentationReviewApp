783e436a365bbe36997163b46aa0e5c95df756ab
simonksays123 <44527417+simonksays123@users.noreply.github.com>
2020-12-05 10:55:21 -0500

    Changed root route to Log In and removed extra routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 Rails.application.routes.draw do
-  get 'reviews/create'
-  get 'reviews/new'
-  get 'reviews/show'
   get 'presentation_student/new'
   get 'presentation_student/show'
   get 'presentation_student/create'
-  get 'presentation_section/new'
-  get 'presentation_section/show'
-  get 'presentation_section/create'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'users#new'
+  root 'sessions#new'
+  get 'sessions/new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+
   post '/courses/new', to: 'courses#create'
   post '/courses/:id', to: 'sections#create'
 
@@ -39,7 +36,6 @@ Rails.application.routes.draw do
 
   # delete a presentation as an instructor
   delete '/presentations/:id', to: 'presentations#destroy'
-  get 'sessions/new'
 
   resources :users
   resources :courses
