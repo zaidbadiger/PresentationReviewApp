d5c8542606228bc870e2cb74071c96e35240b2c7
simonksays123 <44527417+simonksays123@users.noreply.github.com>
2020-12-05 10:28:59 -0500

    Formatted routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,38 +8,42 @@ Rails.application.routes.draw do
   get 'presentation_section/new'
   get 'presentation_section/show'
   get 'presentation_section/create'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#new'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-  post   '/courses/new', to: 'courses#create'
-  post  '/courses/:id', to: 'sections#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  post '/courses/new', to: 'courses#create'
+  post '/courses/:id', to: 'sections#create'
 
   # Roster routes
-  get '/sections/:section_id/rosters', to:'rosters#show'
-  get '/sections/:section_id/rosters/add', to:'rosters#new'
-  post '/sections/:section_id/rosters/add', to:'rosters#create'
+  get '/sections/:section_id/rosters', to: 'rosters#show'
+  get '/sections/:section_id/rosters/add', to: 'rosters#new'
+  post '/sections/:section_id/rosters/add', to: 'rosters#create'
+
   # add presentation to section
-  get '/courses/:id/sections/:id/presentations/:id', to:'presentations#show'
-  post '/courses/:id/sections/:id/presentations/:id', to:'presentations#create'
+  get '/courses/:id/sections/:id/presentations/:id', to: 'presentations#show'
+  post '/courses/:id/sections/:id/presentations/:id', to: 'presentations#create'
+
   # add student to presentation
-  get '/sections/:id/presentations/:id', to:'presentation_student#new'
-  post '/sections/:id/presentations/:id', to:'presentation_student#create'
+  get '/sections/:id/presentations/:id', to: 'presentation_student#new'
+  post '/sections/:id/presentations/:id', to: 'presentation_student#create'
+
   # add review
   get '/presentations/:id/reviews/new', to: 'reviews#new'
   post '/presentations/:id/reviews/new', to: 'reviews#create'
+
   # list reviews for a presentation
   get '/reviews/:id', to: 'reviews#index'
+
   # delete a presentation as an instructor
   delete '/presentations/:id', to: 'presentations#destroy'
   get 'sessions/new'
-  
-  
+
   resources :users
   resources :courses
   resources :sections
   resources :presentations
   resources :reviews
-
 end
