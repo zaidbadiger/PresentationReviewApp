0b6f4b78a8400e89a861803577d456f000f6c66e
simonksays123 <44527417+simonksays123@users.noreply.github.com>
2020-12-05 20:23:24 -0500

    Resolving rubocop issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                    :password_confirmation, :is_instructor)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :is_instructor)
     end
 end
