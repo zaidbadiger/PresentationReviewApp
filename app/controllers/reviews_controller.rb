4349d971ffca9fb65d5e777c66b335070fa57ae4
Rempala5 <rempala.5@buckeyemail.osu.edu>
2020-12-05 21:02:59 -0500

    Added documentation for roster controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,16 @@
 #=begin
-# Controller responsible for handeling Reviews for Presentation
+# Controller responsible for handeling Reviews for Presentation from User model
 #=end
 class ReviewsController < ApplicationController
 
-  
+  # instantiate instance for presentation and current user
+
   def new
     @presentation = Presentation.find(params[:id])
     @user = current_user
   end
+
+  # create new instance of review for current presentation instance
 
   def create
     @review = Presentation.find(params[:id]).reviews.build(reviews_params)
@@ -16,12 +19,17 @@ class ReviewsController < ApplicationController
     redirect_to current_user
   end
 
+  # instantiates tuples for join table of reviews and User model
+
   def index
     @user = current_user
     @presentation = Presentation.find(params[:id])
   end
 
   private
+
+  # returns attributes for reviews instance from form data of post request
+
   def reviews_params
     params.permit(:score, :comment)
   end
--- app/controllers/rosters_controller.rb
+++ app/controllers/rosters_controller.rb
@@ -1,15 +1,30 @@
+#=begin
+# responsible for handeling roster functionality. Implements many to many
+# relationship between model User and model Section
+#=end
 class RostersController < ApplicationController
+  # instantiates instance of the current user logged in
+
   def new
     @user = current_user
   end
+
+  # shows current list of students assigned to section and provides form to add new student
 
   def show
     @section = Section.find(params[:section_id])
     @user_list = @section.users
   end
 
+  # assigns student to current section instance
+
   def create
     @student = User.find_by(email: params[:email])
+
+    #=begin
+    # being if the selected student does not exist do not add and redirect back.
+    # else assign student to section and record in roster instance 
+    #=end
     unless @student.nil?
       if Roster.find_by(section_id: params[:section_id], user_id: @student.id).nil?
         @roster_instance = Roster.create(section_id: params[:section_id], user_id: @student.id)
@@ -18,6 +33,8 @@ class RostersController < ApplicationController
     end
     redirect_to "/sections/#{params[:section_id]}/rosters"
   end
+
+  # remove student from current section
 
   def destroy
     delete_roster(params[:roster_id])
