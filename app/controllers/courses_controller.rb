159550cd3d095c316471124a1ed23fa8f8cd7fbb
Rempala5 <rempala.5@buckeyemail.osu.edu>
2020-12-05 19:54:56 -0500

    Added documentation for Courses_controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,10 +1,21 @@
+# frozen_string_literal: false
+
+#=begin
+# Controller responsible for handeling interactions between
+# views rendering data from Courses table and user
+#=end
 class CoursesController < ApplicationController
+  # include helper functions
   include CoursesHelper
+
+  # instantiates instance variables
 
   def new
     @course = Course.new
     @user = current_user
   end
+
+  # call to delete courses and redirect to user view
 
   def destroy
     delete_course(params[:course_id])
@@ -12,8 +23,7 @@ class CoursesController < ApplicationController
     redirect_to current_user
   end
 
-  def show
-  end
+  # call to create a new course and add to Course model
 
   def create
     @course = Course.new(course_params)
@@ -24,10 +34,14 @@ class CoursesController < ApplicationController
 
   private
 
-    def course_params
-      result = params.require(:course).permit(:name)
-      result[:user_id] = current_user.id
-      result
-    end
+  #=begin
+  # return parameters submitted from form data as post request
+  # to build an instance of course
+  #=end
 
+  def course_params
+    result = params.require(:course).permit(:name)
+    result[:user_id] = current_user.id
+    result
+  end
 end
