a103c71d8b080df261da0398a90b403148257bd6
Rempala5 <rempala.5@buckeyemail.osu.edu>
2020-12-05 20:41:16 -0500

    Added documentation for presenters_controller

--- app/controllers/presenters_controller.rb
+++ app/controllers/presenters_controller.rb
@@ -1,13 +1,30 @@
+#=begin
+# Controller responsible for handeling interactions with Presenter
+# model and Presentation
+#=end
 class PresentersController < ApplicationController
+  # module PresentationsHelper defined in helpers directory.
+
   include PresentersHelper
 
+  #=begin
+  # instantiate instances for user currently logged in.
+  # instantites instance for presentation currently being assigned presenter
+  #=end
   def new
     @user = current_user
     @current_presentation = Presentation.find(params[:presentation_id])
   end
 
+  #=begin
+  # creates instance of presentation for a particular presentation.
+  # redirects to view of current users's Presentation instance
+  #=end
   def create
     @presentation_user = User.find_by(email: params[:email])
+    #=begin reload page if the current user assigned to presentation could not be found or not in roster.
+    # Else find User and assign to presentation instance
+    #=end
     unless @presentation_user.nil? || Roster.find_by(section_id: params[:section_id], user_id: @presentation_user.id).nil?
       if Presenter.find_by(presentation_id: params[:presentation_id], user_id: @presentation_user.id).nil?
         @presenter_instance = Presenter.create(presentation_id: params[:presentation_id], user_id: @presentation_user.id)
@@ -16,6 +33,8 @@ class PresentersController < ApplicationController
     end
     redirect_to "/sections/#{params[:section_id]}/presentations/#{params[:presentation_id]}"
   end
+
+  # delete presenter for current presentation instance
 
   def destroy
     delete_presenter(params[:presenter_id])
