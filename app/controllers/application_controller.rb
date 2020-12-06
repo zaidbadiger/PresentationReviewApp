674c4a61cd57de4e1306b7422df5bb484b0faff2
simonksays123 <44527417+simonksays123@users.noreply.github.com>
2020-12-05 22:47:31 -0500

    Added documentation and fixed rubocop issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
+# =begin
+#   This controller is inherited by all other controllers
+#   Contains includes on basic helpers wanted in all controllers
+# =end
 class ApplicationController < ActionController::Base
   include SessionsHelper
   include CoursesHelper
