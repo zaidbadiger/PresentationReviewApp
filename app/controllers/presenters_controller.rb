c8131ff7e0a1baaf783322a6a3281450b8a0f68e
simonksays123 <44527417+simonksays123@users.noreply.github.com>
2020-12-05 22:15:06 -0500

    Refactored presenters#create

--- app/controllers/presenters_controller.rb
+++ app/controllers/presenters_controller.rb
@@ -22,16 +22,16 @@ class PresentersController < ApplicationController
   #=end
   def create
     @presentation_user = User.find_by(email: params[:email])
+    user_id = @presentation_user.id
+    pres_id = params[:presentation_id]
+    sect_id = params[:section_id]
     #=begin reload page if the current user assigned to presentation could not be found or not in roster.
     # Else find User and assign to presentation instance
     #=end
-    unless @presentation_user.nil? || Roster.find_by(section_id: params[:section_id], user_id: @presentation_user.id).nil?
-      if Presenter.find_by(presentation_id: params[:presentation_id], user_id: @presentation_user.id).nil?
-        @presenter_instance = Presenter.create(presentation_id: params[:presentation_id], user_id: @presentation_user.id)
-        @presenter_instance.save!
-      end
+    unless @presentation_user.nil? || Roster.find_by(section_id: sect_id, user_id: user_id).nil?
+      Presenter.create(presentation_id: pres_id, user_id: user_id).save! if Presenter.find_by(presentation_id: pres_id, user_id: user_id).nil?
     end
-    redirect_to "/sections/#{params[:section_id]}/presentations/#{params[:presentation_id]}"
+    redirect_to "/sections/#{sect_id}/presentations/#{pres_id}"
   end
 
   # delete presenter for current presentation instance
