28cfceb04fcc521dad615c502bbefca4516bff75
Rempala5 <rempala.5@buckeyemail.osu.edu>
2020-12-05 20:44:12 -0500

    Adding doc for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,19 @@
+#=begin
+# Controller responsible for handeling Reviews for Presentation
+#=end
 class ReviewsController < ApplicationController
+
+  
+  def new
+    @presentation = Presentation.find(params[:id])
+    @user = current_user
+  end
+
   def create
     @review = Presentation.find(params[:id]).reviews.build(reviews_params)
     @review.user_id = current_user.id
     @review.save
     redirect_to current_user
-  end
-
-  def new
-    @presentation = Presentation.find(params[:id])
-    @user = current_user
   end
 
   def index
